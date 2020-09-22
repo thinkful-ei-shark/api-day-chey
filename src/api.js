63bf35d8bfa2e51da486e0ea29a56b9ba7c55306
Marc Trius <derpayatz@gmail.com>
2020-09-22 10:35:38 -0500

    create and test api module

---
+++ src/api.js
@@ -0,0 +1,9 @@
+const baseUrl = 'https://thinkful-list-api.herokuapp.com/mashinke';
+
+const getItems = function() {
+  return Promise.resolve('A successful response!');
+};
+
+export default {
+  getItems
+};
--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import $ from 'jquery';
 import 'normalize.css';
 import './index.css';
 
+import api from './api';
+
 import shoppingList from './shopping-list';
 
 const main = function () {
+  api.getItems()
+    .then(res => console.log(res));
+  
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
